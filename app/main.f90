ef3282ba853644faa03eb02f19c4878766307f01
Lucas <lucasbarbr@outlook.com>
2023-08-24 15:10:05 +0200

    ajout emplacement script

--- app/main.f90
+++ app/main.f90
@@ -1,6 +1,9 @@
 program main
   use appmaker, only: create_dirs, create_file
+  use filehandler, only: GetFileName
   character(256) :: appPath
+  character(256) :: ScriptPath
+  character(len=:), allocatable :: ScriptName
   character(256) :: commandPath
   character(256) :: commandContents
   character(256) :: iconPath
@@ -9,24 +12,27 @@ program main
   character(256) :: appName
 
   ! Vérifier si le nom de l'application est fourni en argument
-  if (command_argument_count() == 0) then
-      print *, "Veuillez fournir le nom de l'application en argument de commande."
+  if (command_argument_count() < 2) then
+      print *, "Veuillez fournir le nom de l'application et l'emplacement du script en argument de commande."
       stop
   end if
 
   ! Récupérer le nom de l'application à partir des arguments
   call get_command_argument(1, value=appName)
-
+  call get_command_argument(2, value=scriptPath)
   ! Chemin vers le dossier de l'application
   ! appPath = "/Applications/" // trim(appName) // ".app/"
   appPath = trim(appName) // ".app/"
+  call GetFileName(scriptPath, ScriptName)
 
+  
   ! Chemin vers le fichier de commande
   commandPath = trim(appPath) // "Contents/MacOS/" // trim(appName) // "/run.command"
+
   commandContents =  & 
                     '#!/bin/bash\n\n' // &
                     'cd "$(dirname "$0")"\n' // &
-                    './test.sh\n'
+                    './' // ScriptName
   
   ! Chemin vers le fichier d'icône (facultatif)
   iconPath = "Icon.icns"
@@ -53,13 +59,17 @@ program main
   ! call system("cp " // trim(iconPath) // " " // trim(appPath) // "Contents/Resources/Icon.icns")
   ! call system("echo '" // trim(plistContents) // "' > " // trim(appPath) // "Contents/Info.plist")
   call create_dirs(appName)
+  call system("cp " // ScriptPath // " " // trim(appPath) // "Contents/MacOS/" // trim(appName) // "/" // ScriptName)
   call create_file(filePath=commandPath, fileContent=commandContents)
   call create_file(filePath=plistPath, fileContent=plistContents)
+  
   ! call system("touch " // trim(appPath) // trim(commandPath))
   ! call system("touch " // trim(appPath) // trim(commandPath))
   ! call system("cp " // trim(appPath) // trim(commandPath) // " " // trim(appPath) // "Contents/MacOS/" // trim(appName))
-  call system("cp " // trim(iconPath) // " " // trim(appPath) // "Contents/Resources/Icon.icns")
+  call system("touch " // trim(appPath) // "Contents/Resources/Icon.icns")
   ! call system("echo '" // trim(plistContents) // "' > " // trim(appPath) // "Contents/Info.plist")
 
   print *, "Application créée avec succès!"
+  call system("tree " // appPath)
+  deallocate(ScriptName)
 end program main
